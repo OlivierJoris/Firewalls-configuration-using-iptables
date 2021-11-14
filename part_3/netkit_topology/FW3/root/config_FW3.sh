7facf6ec11ea63dc2dc188917b2981686e4a5e5c
Olivier Joris <joris.olivier@hotmail.com>
2021-11-14 17:01:02 +0100

    fw2 implemented

--- part_3/netkit_topology/FW3/root/config_FW3.sh
+++ part_3/netkit_topology/FW3/root/config_FW3.sh
@@ -4,34 +4,37 @@
 iptables -P INPUT DROP
 iptables -P OUTPUT DROP
 
+## Stateful firewall
+iptables -A FORWARD -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+
 ## Incoming traffic z-nfs 
 
 # 1. HONEYPOT to NFS (portmapper)
-iptables -A FORWARD -s 192.168.3.2 -d 10.10.3.2 -p tcp --dport 111 -j ACCEPT
+iptables -A FORWARD -s 192.168.3.2 -d 10.10.3.2 -p tcp --dport 111 -m conntrack --ctstate NEW -j ACCEPT
 # 2. HONEYPOT to NFS (portmapper)
-iptables -A FORWARD -s 192.168.3.2 -d 10.10.3.2 -p udp --dport 111 -j ACCEPT
+iptables -A FORWARD -s 192.168.3.2 -d 10.10.3.2 -p udp --dport 111 -m conntrack --ctstate NEW -j ACCEPT
 # 3. HONEYPOT to NFS (status)
-iptables -A FORWARD -s 192.168.3.2 -d 10.10.3.2 -p tcp --dport 2046 -j ACCEPT
+iptables -A FORWARD -s 192.168.3.2 -d 10.10.3.2 -p tcp --dport 2046 -m conntrack --ctstate NEW -j ACCEPT
 # 4. HONEYPOT to NFS (status)
-iptables -A FORWARD -s 192.168.3.2 -d 10.10.3.2 -p udp --dport 2046 -j ACCEPT
+iptables -A FORWARD -s 192.168.3.2 -d 10.10.3.2 -p udp --dport 2046 -m conntrack --ctstate NEW -j ACCEPT
 # 5. HONEYPOT to NFS (nlockmgr)
-iptables -A FORWARD -s 192.168.3.2 -d 10.10.3.2 -p tcp --dport 2047 -j ACCEPT
+iptables -A FORWARD -s 192.168.3.2 -d 10.10.3.2 -p tcp --dport 2047 -m conntrack --ctstate NEW -j ACCEPT
 # 6. HONEYPOT to NFS (nlockmgr)
-iptables -A FORWARD -s 192.168.3.2 -d 10.10.3.2 -p udp --dport 2047 -j ACCEPT
+iptables -A FORWARD -s 192.168.3.2 -d 10.10.3.2 -p udp --dport 2047 -m conntrack --ctstate NEW -j ACCEPT
 # 7. HONEYPOT to NFS (mountd)
-iptables -A FORWARD -s 192.168.3.2 -d 10.10.3.2 -p tcp --dport 2048 -j ACCEPT
+iptables -A FORWARD -s 192.168.3.2 -d 10.10.3.2 -p tcp --dport 2048 -m conntrack --ctstate NEW -j ACCEPT
 # 8. HONEYPOT to NFS (mountd)
-iptables -A FORWARD -s 192.168.3.2 -d 10.10.3.2 -p udp --dport 2048 -j ACCEPT
+iptables -A FORWARD -s 192.168.3.2 -d 10.10.3.2 -p udp --dport 2048 -m conntrack --ctstate NEW -j ACCEPT
 # 9. HONEYPOT to NFS
-iptables -A FORWARD -s 192.168.3.2 -d 10.10.3.2 -p tcp --dport 2049 -j ACCEPT
+iptables -A FORWARD -s 192.168.3.2 -d 10.10.3.2 -p tcp --dport 2049 -m conntrack --ctstate NEW -j ACCEPT
 # 10. HONEYPOT to NFS
-iptables -A FORWARD -s 192.168.3.2 -d 10.10.3.2 -p udp --dport 2049 -j ACCEPT
+iptables -A FORWARD -s 192.168.3.2 -d 10.10.3.2 -p udp --dport 2049 -m conntrack --ctstate NEW -j ACCEPT
 # 11. U3 to RSYNC
-iptables -A FORWARD -s 192.168.3.3 -d 10.10.3.3 -p tcp --dport 873 -j ACCEPT
+iptables -A FORWARD -s 192.168.3.3 -d 10.10.3.3 -p tcp --dport 873 -m conntrack --ctstate NEW -j ACCEPT
 # 12. U3 to RSYNC (secured)
-iptables -A FORWARD -s 192.168.3.3 -d 10.10.3.3 -p tcp --dport 22 -j ACCEPT
+iptables -A FORWARD -s 192.168.3.3 -d 10.10.3.3 -p tcp --dport 22 -m conntrack --ctstate NEW -j ACCEPT
 # 13. SSH to RSYNC
-iptables -A FORWARD -s 10.10.4.6 -d 10.10.3.3 -p tcp --dport 22 -j ACCEPT
+iptables -A FORWARD -s 10.10.4.6 -d 10.10.3.3 -p tcp --dport 22 -m conntrack --ctstate NEW -j ACCEPT
 # 14. input deny
 iptables -A FORWARD -d 10.10.3.0/24 -j DROP
 
@@ -43,14 +46,14 @@ iptables -A FORWARD -s 10.10.3.0/24 -j DROP
 ## Incoming traffic z-u3
 
 # 16. SSH to HONEYPOT
-iptables -A FORWARD -s 10.10.4.6 -d 192.168.3.2 -p tcp --dport 22 -j ACCEPT
+iptables -A FORWARD -s 10.10.4.6 -d 192.168.3.2 -p tcp --dport 22 -m conntrack --ctstate NEW -j ACCEPT
 # 17. input deny
 iptables -A FORWARD -d 192.168.3.0/24 -j DROP
 
 ## Outgoing traffic z-u3
 
 # 18. U3 to SSH
-iptables -A FORWARD -s 192.168.3.3 -d 192.168.3.0/24 -p tcp --dport 22 -j DROP
+iptables -A FORWARD -s 192.168.3.3 -d 192.168.3.0/24 -p tcp --dport 22 -m conntrack --ctstate NEW -j ACCEPT
 # 19. output deny
 iptables -A FORWARD -s 192.168.3.0/24 -j DROP
 
